6c5f23dea6e7aa5a93bb9f6db03d57ec9d885e63
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-12 21:51:45 +0700

    TT-54: add db migration module

---
+++ rgo-tt-authentication-db-migration/src/main/resources/db/migration/V1.0.0__create_client.sql
@@ -0,0 +1,13 @@
+CREATE SEQUENCE client_sequence;
+
+CREATE TABLE client (
+    PRIMARY KEY(entity_id),
+    entity_id          BIGINT       DEFAULT nextval('client_sequence'),
+    email              VARCHAR(128) NOT NULL,
+    password           VARCHAR(128) NOT NULL,
+    created_date       TIMESTAMP    DEFAULT (now() AT TIME ZONE 'UTC'),
+    last_modified_date TIMESTAMP    DEFAULT (now() AT TIME ZONE 'UTC')
+);
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON client TO ${appRole};
+GRANT SELECT ON client TO ${readerRole};
