426cb1cda8f0d0205e400834808cbe317e1b8b54
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-16 19:46:11 +0700

    rename container name

--- rgo-tt-client-db-migration/docker-deploy.cmd
+++ rgo-tt-client-db-migration/docker-deploy.cmd
@@ -1,5 +1,6 @@
 docker-compose up -d
 docker rmi michisig/tt-client-db:%1
-docker container commit native-db michisig/tt-client-db:%1
+docker container commit client-db michisig/tt-client-db:%1
 docker image push michisig/tt-client-db:%1
 docker-compose stop
+docker rmi --force client-db:latest
