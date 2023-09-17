98c9ceb25d8682447372a5950e1ae8dc4f76040e
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-17 12:19:38 +0700

    remove docker container

--- rgo-tt-user-db-migration/docker-deploy.cmd
+++ rgo-tt-user-db-migration/docker-deploy.cmd
@@ -3,4 +3,5 @@ docker rmi michisig/tt-client-db:%1
 docker container commit client-db michisig/tt-client-db:%1
 docker image push michisig/tt-client-db:%1
 docker-compose stop
-docker rmi --force client-db:latest
+docker rm client-db
+docker rmi client-db:latest
