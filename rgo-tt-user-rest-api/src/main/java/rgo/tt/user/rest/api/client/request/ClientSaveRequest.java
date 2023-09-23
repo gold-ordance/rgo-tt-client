585a56fd5adaae00217613887d1304731ea56794
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-23 23:08:28 +0700

    add strip method for request fields

--- rgo-tt-user-rest-api/src/main/java/rgo/tt/user/rest/api/client/request/ClientSaveRequest.java
+++ rgo-tt-user-rest-api/src/main/java/rgo/tt/user/rest/api/client/request/ClientSaveRequest.java
@@ -1,5 +1,7 @@
 package rgo.tt.user.rest.api.client.request;
 
+import static org.apache.commons.lang3.StringUtils.strip;
+
 public class ClientSaveRequest {
 
     private String email;
@@ -10,7 +12,7 @@ public class ClientSaveRequest {
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email = strip(email);
     }
 
     public String getPassword() {
@@ -18,7 +20,7 @@ public class ClientSaveRequest {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = strip(password);
     }
 
     @Override
