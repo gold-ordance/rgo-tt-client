e1f06788d2eada073d485a7c3fb05309e71f0b0a
evgeny-mordyasov <michisig11i7@gmail.com>
2023-10-21 14:08:33 +0700

    add tests for rate limiter

---
+++ rgo-tt-user-boot/src/test/java/rgo/tt/user/rest/api/client/RateLimiterRestClientServiceTest.java
@@ -0,0 +1,40 @@
+package rgo.tt.user.rest.api.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import rgo.tt.common.armeria.test.ratelimiter.AbstractRateLimiterTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static rgo.tt.common.utils.RandomUtils.randomPositiveLong;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RateLimiterRestClientServiceTest extends AbstractRateLimiterTest {
+
+    private static final int PORT = 8081;
+    private static final String BASE_URL = "/clients";
+
+    protected RateLimiterRestClientServiceTest() {
+        super(PORT);
+    }
+
+    @Test
+    void findAll() {
+        boolean isRequestRejected = isRequestRejected(() -> get(BASE_URL));
+        assertThat(isRequestRejected).isTrue();
+    }
+
+    @Test
+    void findByEntityId() {
+        long entityId = randomPositiveLong();
+        boolean isRequestRejected = isRequestRejected(() -> get(BASE_URL + "/" + entityId));
+        assertThat(isRequestRejected).isTrue();
+    }
+
+    @Test
+    void save() {
+        boolean isRequestRejected = isRequestRejected(() -> post(BASE_URL));
+        assertThat(isRequestRejected).isTrue();
+    }
+}
